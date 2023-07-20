85d66a588f434e5536c6b7e27fc79d8e57cab796
Keagan Hartman <keaganhartmandev@gmail.com>
2023-07-19 23:40:28 -0400

    Initial Setup Commit
    Setting up and figuring out which files to ignore in initial push to new repository.

---
+++ Solution/Arrays and Hashing/Contains_Duplicate.h
@@ -0,0 +1,8 @@
+#pragma once
+#include "includes.h"
+class Contains_Duplicate
+{
+	// Given an integer array (nums), return (true) if any value appears at least twice in the
+	// array, and return (false) if every element is distinct.
+};
+
