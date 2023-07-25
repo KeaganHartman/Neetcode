af5856ebdc81119e9ab0f3eba8a12ff0538daa3c
Keagan Hartman <keaganhartmandev@gmail.com>
2023-07-25 12:18:50 -0400

    Product of Array except Self Setup

---
+++ Solution/Arrays and Hashing/src/Product_of_Array_Except_Self.cpp
@@ -0,0 +1,16 @@
+#include "Product_of_Array_Except_Self.h"
+
+Product_of_Array_Except_Self::Product_of_Array_Except_Self()
+{
+    vector<int> input = { 1, 2, 3, 4 };
+    vector<int> output = productExceptSelf(input);
+    for (size_t i = 0; i < output.size(); i++)
+    {
+        cout << output[i] << " ";
+    }
+}
+
+vector<int> Product_of_Array_Except_Self::productExceptSelf(vector<int>& nums)
+{
+    
+}
---
+++ Solution/Arrays and Hashing/src/Product_of_Array_Except_Self.h
@@ -0,0 +1,10 @@
+#pragma once
+#include "includes.h"
+class Product_of_Array_Except_Self
+{
+public:
+	Product_of_Array_Except_Self();
+private:
+	vector<int> productExceptSelf(vector<int>& nums);
+};
+
