f670d3e9710e48a24d5af06a1c91342ff561445b
Keagan Hartman <keaganhartmandev@gmail.com>
2023-07-25 12:19:13 -0400

    Product of Array except self setup

--- Solution/Arrays and Hashing/src/Product_of_Array_Except_Self.h
+++ Solution/Arrays and Hashing/src/Product_of_Array_Except_Self.h
@@ -5,6 +5,9 @@ class Product_of_Array_Except_Self
 public:
 	Product_of_Array_Except_Self();
 private:
+	/*Given an integer array (nums), return an array (answer) such that (answer[i]) is equal to the product of all
+	the elements of nums except (nums[i]).
+	No Division. O(n)*/
 	vector<int> productExceptSelf(vector<int>& nums);
 };
 
