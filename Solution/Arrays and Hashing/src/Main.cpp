a4d6da89c039698d6d8efb4bf66b8c2f3adfde71
Keagan Hartman <keaganhartmandev@gmail.com>
2023-07-26 12:50:18 -0400

    Product of Array except Self Solved
    Not as optimized as my first solution submission. But works nonetheless.

--- Solution/Arrays and Hashing/src/Main.cpp
+++ Solution/Arrays and Hashing/src/Main.cpp
@@ -3,9 +3,10 @@
 #include "Two_Sum.h"
 #include "Group_Anagrams.h"
 #include "Top_K_Frequent_Elements.h"
+#include "Product_of_Array_Except_Self.h"
 int main()
 {
-	Top_K_Frequent_Elements* test = new Top_K_Frequent_Elements();
+	Product_of_Array_Except_Self* test = new Product_of_Array_Except_Self();
 	delete test;
 	cin.get();
 }
--- Solution/Arrays and Hashing/src/Product_of_Array_Except_Self.cpp
+++ Solution/Arrays and Hashing/src/Product_of_Array_Except_Self.cpp
@@ -12,5 +12,39 @@ Product_of_Array_Except_Self::Product_of_Array_Except_Self()
 
 vector<int> Product_of_Array_Except_Self::productExceptSelf(vector<int>& nums)
 {
-    
+    // Create Prefix Products List [1, 2, 6, 24]
+    vector<int> prefix_products;
+    int prod = 1;
+    int len = nums.size();
+    for (int i = 0; i < len; i++) {
+        prod *= nums[i];
+        prefix_products.push_back(prod);
+    }
+
+    // Create Postfix Products List [24, 24, 12, 4]
+    vector<int> postfix_products;
+    postfix_products.resize(nums.size());
+    prod = 1;
+    for (int i = len - 1; i >= 0; i--)
+    {
+        prod *= nums[i];
+        postfix_products[i] = prod;
+    }
+
+    // Last Loop Calculates Return Vector
+    vector<int> rVector;
+    for (int i = 0; i < len; i++)
+    {
+        int temp = 1;
+        // Check Right
+        if (i + 1 < len)
+            temp *= postfix_products[i + 1];
+        // Check Left
+        if (i - 1 >= 0)
+            temp *= prefix_products[i - 1];
+
+        rVector.push_back(temp);
+    }
+
+    return rVector;
 }
